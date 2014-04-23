444e90cf1fa03fe2abfe011514bddf51a3498556
Andrey Koval <koval.andrey@gmail.com>
2014-04-23 12:47:46 -0500

    en route to wrap up

---
+++ Manipulation/temp.R
@@ -0,0 +1,45 @@
+# Clear memory from previous runs
+base::rm(list=base::ls(all=TRUE))
+
+#####################################
+## @knitr LoadData
+
+###################################
+# Load the necessary packages.
+base::require(base)
+base::require(knitr)
+base::require(markdown)
+base::require(testit)
+base::require(plyr)
+base::require(reshape2)
+base::require(stringr)
+
+
+###################################
+# Build the reports
+pathsReports <- base::file.path("./", c("README.md"))
+for( pathRmd in pathsReports ) {
+  pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+  pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
+  knitr::knit(input=pathRmd, output=pathMd)
+  markdown::markdownToHTML(file=pathMd, output=pathHtml)
+}
+
+
+pathMd <- base::file.path("./", c("README.md"))
+pathHtml <- base::gsub(pattern=".md$", replacement=".html", x=pathMd)
+markdown::markdownToHTML(file=pathMd, output=pathHtml)
+
+
+
+# Print this *.Rmd as *.md, then turn *.md into *.html
+pathRmd<-base::file.path ("./Documentation/manipulation","ImportAndClean.Rmd")
+pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
+knitr::knit(input=pathRmd, output=pathMd)
+markdown::markdownToHTML(file=pathMd, output=pathHtml)
+
+# ColumnRenameHeadstart <- function( d ) {
+  cat(paste0(", \"", colnames(d), "\"", " = ", "\"", colnames(d), "\"", "\n")) #Gives a headstart to plyr::rename
+# }
+# ColumnRenameHeadstart(dsSource)
