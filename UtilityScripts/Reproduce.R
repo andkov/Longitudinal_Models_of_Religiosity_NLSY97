d1c0dca25dd6d3c6036188b8e8ebf344feae4b6f
Andrey Koval <koval.andrey@gmail.com>
2014-04-23 21:34:34 -0500

    first break through

--- UtilityScripts/Reproduce.R
+++ UtilityScripts/Reproduce.R
@@ -32,19 +32,20 @@ base::require(knitr)
 base::require(markdown)
 base::require(testit)
 
-# #Report Files:
-pathsReports <- base::file.path(getwd(),"Data", c("Derive_dsL_from_Extract.Rmd"))
-
-# Verify the necessary path can be found.
-# Report Files:
-testit::assert("The knitr Rmd files should exist.", base::file.exists(pathsReports))
-
-###################################
-# Build the reports
-# pathsReports <- base::file.path("./Documentation", c("ImportAndClean.Rmd"))
-for( pathRmd in pathsReports ) {
-  pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
-  pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
-  knitr::knit(input=pathRmd, output=pathMd)
-  markdown::markdownToHTML(file=pathMd, output=pathHtml)
-}
+### Don't use this report technology yet. buggy
+# # #Report Files:
+# pathsReports <- base::file.path(getwd(),"Data", c("Derive_dsL_from_Extract.Rmd"))
+# 
+# # Verify the necessary path can be found.
+# # Report Files:
+# testit::assert("The knitr Rmd files should exist.", base::file.exists(pathsReports))
+# 
+# ###################################
+# # Build the reports
+# # pathsReports <- base::file.path("./Documentation", c("ImportAndClean.Rmd"))
+# for( pathRmd in pathsReports ) {
+#   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+#   pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
+#   knitr::knit(input=pathRmd, output=pathMd)
+#   markdown::markdownToHTML(file=pathMd, output=pathHtml)
+# }
