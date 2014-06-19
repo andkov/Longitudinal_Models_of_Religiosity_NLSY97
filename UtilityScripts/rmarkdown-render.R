954d93005735e223de876656e5a0083619ce611d
Andrey Koval <koval.andrey@gmail.com>
2014-06-18 22:49:05 -0500

    moved to new R-studio version
    first files

---
+++ UtilityScripts/rmarkdown-render.R
@@ -0,0 +1,25 @@
+
+############# Derive dsL from Extract ###################
+# Run this code manualy, connection doesn't work yet
+pathDerive <- base::file.path("./Data/Derive_dsL_from_Extract.Rmd")
+pathDescriptivesList <- base::file.path("./Models/Descriptives/DescriptivesList.Rmd")
+
+pathDescriptives <- base::file.path("./Models/Descriptives/Descriptives.Rmd")
+pathLCM <- base::file.path("./Models/LCM/LCM.Rmd")
+pathAAA <- base::file.path("./Models/Descriptives/AttendAndAge.Rmd")
+pathMetrics<- base::file.path("./Models/Descriptives/Metrics.Rmd")
+
+allreps<- c(pathDerive, pathDescriptives, pathLCM, pathAAA, pathLabelFactors )
+
+pathsReports <-c(pathMetrics)
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathsReports))
+
+# Build the reports
+for( pathRmd in pathsReports ) {
+  pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+  pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
+  #   knitr::knit(input=pathRmd, output=pathMd)
+  rmarkdown::render(input = pathRmd, 
+                    output_format=c( "html_document","md_document"),
+                    clean=TRUE)
+}
