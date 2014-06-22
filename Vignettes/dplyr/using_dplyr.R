f0845c96a6e5f211a6eaa6202c11f8eb8b7b4c87
Andrey Koval <koval.andrey@gmail.com>
2014-06-22 04:28:55 -0500

    dplyr on board

--- Vignettes/dplyr/using_dplyr.R
+++ Vignettes/dplyr/using_dplyr.R
@@ -25,12 +25,11 @@ dsL<-readRDS("./Data/Derived/dsL.rds")
 ############################
 ## @knitr AnalysisChunk03
 
-ignette on using dplyr 
 
 
 
 
-
+# vignette("markdown-examples", package="markdown")
 
 
 
@@ -38,17 +37,5 @@ ignette on using dplyr
 
 
 
-# p<- ggplot(ds, aes(x=factor(year), fill=attendF))
-# p<- p+ geom_bar(position="fill")
-# p<- p+ scale_fill_manual(values = attcol8,
-#                          name="Response category" )
-# p<- p+ scale_y_continuous("Prevalence: proportion of total",
-#                           limits=c(0, 1),
-#                           breaks=c(.1,.2,.3,.4,.5,.6,.7,.8,.9,1))
-# p<- p+ scale_x_discrete("Waves of measurement",
-#                         limits=as.character(c(2000:2011)))
-# p<- p+ labs(title=paste0("In the past year, how often have you attended a worship service?"))
-# # p<- p+ geom_text(aes(y=attendF), vjust=1.5, colour="white")
-# p
-# 
+
 
