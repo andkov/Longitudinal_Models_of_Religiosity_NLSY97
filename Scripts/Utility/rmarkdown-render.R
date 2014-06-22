91e1f9eebe8513e2531ac2e19829cd06afb89459
Andrey Koval <koval.andrey@gmail.com>
2014-06-22 15:38:29 -0500

    messed up

--- Scripts/Utility/rmarkdown-render.R
+++ Scripts/Utility/rmarkdown-render.R
@@ -1,18 +1,19 @@
 
 ########## Production of reports from .Rmd files ###
-
+require(dplyr)
 
 pathDerive <- base::file.path("./Data/Derive_dsL_from_Extract.Rmd")
 pathDatabox <- base::file.path("./Models/Descriptives/Databox.Rmd")
 pathMetrics<- base::file.path("./Models/Descriptives/Metrics.Rmd")
 pathLCM <- base::file.path("./Models/LCM/LCM.Rmd")
-pathCurQuest <- base::file.path("./Scripts/Questions/Removing_NA/Removing_NA_from_distributions_ggplot.Rmd")
+pathDataManipulation <- base::file.path("./Vignettes/Data_Manipulation_dplyr/Data_Manipulation_Guide.Rmd")
+pathRemoveNA<- base::file.path("./Scripts/Questions/Removing_NA/Removing_NA_from_distributions_ggplot.Rmd")
 
 ########## Define groups of reports 
 descriptives<-c(pathDatabox,pathMetrics)
-allreps<- c(pathDerive, pathDatabox,pathMetrics, pathLCM )
+allreps<- c(pathDerive, pathDatabox, pathMetrics, pathLCM, pathDataManipulation,  )
 # Place report paths HERE:
-buildthese <- c(pathMetrics)
+buildthese <- c(pathDataManipulation)
 
 
 testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
@@ -24,7 +25,7 @@ for( pathRmd in buildthese ) {
                       "md_document"
                       ,"html_document"
 #                       ,"word_document"
-#                       "pdf_document"
+#                       ,"pdf_document"
                                     ),
 #                     output_file=pathMd,
                     clean=TRUE)
@@ -32,3 +33,9 @@ for( pathRmd in buildthese ) {
 
 # library(extrafont)
 # font_import()
+
+pathSource<- file.path("./Vignettes/dplyr/Data_Manipulation_Guide")
+pathHtml<- paste0(pathSource,'.html')
+pathPdf<- paste0(pathSource,".pdf")
+
+system("pandoc -s pathHtml -o pathPdf")
