bcd5008a21a0104b61d8f26c90416d80b9dcab59
Andrey Koval <koval.andrey@gmail.com>
2014-06-22 15:55:39 -0500

    trying to correct

--- Scripts/Utility/rmarkdown-render.R
+++ Scripts/Utility/rmarkdown-render.R
@@ -1,18 +1,24 @@
 
 ########## Production of reports from .Rmd files ###
-
+require(dplyr)
 
 pathDerive <- base::file.path("./Data/Derive_dsL_from_Extract.Rmd")
-pathDatabox <- base::file.path("./Models/Descriptives/Databox.Rmd")
+pathManipulate <- base::file.path("./Vignettes/dplyr/Data_Manipulation_Guide.Rmd")
 pathMetrics<- base::file.path("./Models/Descriptives/Metrics.Rmd")
+pathDatabox <- base::file.path("./Models/Descriptives/Databox.Rmd")
+
+pathRemoveNA<- base::file.path("./Scripts/Questions/Removing_NA/Removing_NA_from_distributions_ggplot.Rmd")
+
 pathLCM <- base::file.path("./Models/LCM/LCM.Rmd")
-pathCurQuest <- base::file.path("./Scripts/Questions/Removing_NA/Removing_NA_from_distributions_ggplot.Rmd")
+
+
+
 
 ########## Define groups of reports 
 descriptives<-c(pathDatabox,pathMetrics)
-allreps<- c(pathDerive, pathDatabox,pathMetrics, pathLCM )
+allreps<- c(pathDerive, pathDatabox,pathMetrics, pathLCM, pathRemoveNA )
 # Place report paths HERE:
-buildthese <- c(pathMetrics)
+buildthese <- c(pathManipulate)
 
 
 testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
@@ -21,8 +27,8 @@ for( pathRmd in buildthese ) {
 #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
   rmarkdown::render(input = pathRmd, 
                     output_format=c(
-                      "md_document"
-                      ,"html_document"
+#                       "md_document"
+                      "html_document"
 #                       ,"word_document"
 #                       "pdf_document"
                                     ),
