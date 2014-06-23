71204e3a1fa9e3bffac61e9a248bb58143a88402
Andrey Koval <koval.andrey@gmail.com>
2014-06-23 10:55:54 -0500

    adjusting producer

--- Scripts/Utility/rmarkdown-render.R
+++ Scripts/Utility/rmarkdown-render.R
@@ -3,23 +3,26 @@
 require(dplyr)
 
 pathDerive <- base::file.path("./Data/Derive_dsL_from_Extract.Rmd")
-pathManipulate <- base::file.path("./Vignettes/dplyr/Data_Manipulation_Guide.Rmd")
+
 pathMetrics<- base::file.path("./Models/Descriptives/Metrics.Rmd")
 pathDatabox <- base::file.path("./Models/Descriptives/Databox.Rmd")
 
+pathManipulate <- base::file.path("./Vignettes/dplyr/Data_Manipulation_Guide.Rmd")
+pathFigSize <- base::file.path("./Vignettes/fig_size/fig_size.Rmd")
+
 pathRemoveNA<- base::file.path("./Scripts/Questions/Removing_NA/Removing_NA_from_distributions_ggplot.Rmd")
+pathRawVsSum<- base::file.path("./Scripts/Questions/Raw_vs_Summarized/Raw_vs_Summarized.Rmd")
 
 pathLCM <- base::file.path("./Models/LCM/LCM.Rmd")
 
-
-
-
-########## Define groups of reports 
+#  Define groups of reports 
 descriptives<-c(pathDatabox,pathMetrics)
 allreps<- c(pathDerive, pathManipulate,pathMetrics, pathLCM, pathRemoveNA )
-# Place report paths HERE:
-buildthese <- c(allreps)
 
+
+# Place report paths HERE ###########
+buildthese <- c(pathFigSize)##########
+####################################
 
 testit::assert("The knitr Rmd files should exist.", base::file.exists(buildthese))
 # Build the reports
@@ -27,14 +30,12 @@ for( pathRmd in buildthese ) {
 #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
   rmarkdown::render(input = pathRmd, 
                     output_format=c(
-                      "md_document"
-                      ,"html_document"
-#                       ,"word_document"
+                      "html_document"
+                      ,"md_document"
                       ,"pdf_document"
+#                       ,"word_document"
                                     ),
 #                     output_file=pathMd,
                     clean=TRUE)
 }
 
-# library(extrafont)
-# font_import()
