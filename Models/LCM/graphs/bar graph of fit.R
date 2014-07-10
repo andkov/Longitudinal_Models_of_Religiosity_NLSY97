4d655ec62d34d2645cab0d367648f9472f0cb28b
Andrey Koval <koval.andrey@gmail.com>
2014-07-09 20:36:27 -0500

    graphs are ready

--- Models/LCM/graphs/bar graph of fit.R
+++ Models/LCM/graphs/bar graph of fit.R
@@ -1,99 +1,77 @@
-modelsFE <- c(  "m0F", "m1F", "m2F", "m3F", "m4F", "m5F", "m6F", "m7F",
-                "mFa", "mFb", "mFc", "mFd","mFe")
-modelsR1 <- c("m0R1", "m1R1", "m2R1", "m3R1", "m4R1", "m5R1", "m6R1", "m7R1",
-              "mR1a", "mR1b", "mR1c", "mR1d","mR1e")
+###################
+# Read in different REDS files and join them all together
+pathDataDirectory <- file.path("./Models/LCM/models/datasets")
+# filenamePattern <- ".+\\.rds" #All RDS files
+filenamePattern <- "m.{1,}Info\\.rds" #All RDS files
+
+retrievedFilenames <- list.files(path=pathDataDirectory, pattern=filenamePattern)
+filePaths <- file.path(pathDataDirectory, retrievedFilenames)
+
+dsInfo <- readRDS(filePaths[1])
+for( i in 1:length(filePaths) ) {
+  # To debug, change the '6' to some number to isolate the problem: for( i in 2:6 ) {
+  # message("About to read", filePaths[i], "\\")
+  dsInfoSingle <- readRDS(filePaths[i])
+  dsInfo <- plyr::join(x=dsInfo, y=dsInfoSingle, by="Coefficient", type="left", match="all")
+  rm(dsInfoSingle)
+}
+
+modelsFE <- c(  "m0F", "m1F", "m2F", "m3F", "m4F", "m5F", "m6F", "m7F")
+otherFE<- c("mFa", "mFb", "mFc", "mFd","mFe")
+modelsR1 <- c("m0R1", "m1R1", "m2R1", "m3R1", "m4R1", "m5R1", "m6R1", "m7R1")
+otherR1<- c ("mR1a", "mR1b", "mR1c", "mR1d","mR1e")
 modelsR2 <- c(        "m1R2", "m2R2", "m3R2", "m4R2", "m5R2", "m6R2", "m7R2")
 
 modelsR3 <- c(                "m2R3", "m3R3", "m4R3", "m5R3", "m6R3", "m7R3")
 
 modelsR4 <- c(                        "m3R4", "m4R4", "m5R4", "m6R4", "m7R4")
 
-allModels<- c(modelsFE, modelsR1, modelsR2, modelsR3, modelsR4)
-a<- c( c(1:length(allModels)), allModels)
+modelList1<- c(modelsFE, modelsR1, modelsR2, modelsR3, modelsR4, otherFE, otherR1)
 
-a<- c(1:3)
-b<- c("a","b","c")
-d<- c(a,b)
+modelList2<- c( "m0F", "m0R1", 
+                "m1F", "m1R1", "m1R2",
+                "m2F", "m2R1", "m2R2", 
+                "m3F", "m3R1", "m3R2", "m3R3", "m3R4",
+                "m4F", "m4R1", "m4R2", "m4R3", "m4R4",
+                "m5F", "m5R1", "m5R2", "m5R3", "m5R4",
+                "m6F", "m6R1", "m6R2", "m6R3", "m6R4",
+                "m7F", "m7R1", "m7R2", "m7R3", "m7R4",
+                "mFa", "mR1a", "mFb", "mR1b","mFc", "mR1c","mFd", "mR1d", "mFe", "mR1e"    
+                )
+
+
+
 ### graph of comparative fit
 ds<-dsInfo
 head(ds)
+
+
 ds<- reshape2::melt(ds,id.vars=c('Coefficient'))
 ds<-plyr::rename(ds, replace = c( variable = "model"))
-head(ds,9)
+head(ds,10)
 ds<- ds %>% 
-  dplyr::filter(Coefficient %in% c("AIC", "BIC","deviance")) 
-head(ds,6)
-
-# 
-# ds$model<- ordered(allModels)
-# ds$Coefficient<- ordered= c("BIC", "AIC","deviance")
-
-
+  dplyr::filter(Coefficient %in% c( "BIC","AIC","deviance")) 
+head(ds,160)
+ds$Coefficient<- factor(c("BIC","AIC","deviance"))
+# ds$Coefficient<- factor(c("deviance","BIC","AIC"))
+# ds$Coefficient<- factor(c("BIC","deviance","AIC"))
+# ds$Coefficient<- factor(c("deviance","AIC","BIC"))
+# ds$Coefficient<- factor(c("AIC","deviance","BIC"))
+# ds$Coefficient<- factor(c("AIC","BIC","deviance"))
 
 head(ds,9)
 # possible pallets
-colorFit1 <- c("BIC"="#8da0cb", "AIC"="#fc8d62", "deviance"="#66c2a5")
+# colorFit <- c("BIC"="#8da0cb", "AIC"="#fc8d62", "deviance"="#66c2a5")
+colorFit <- c("BIC"="blue", "AIC"="tomato", "deviance"="yellow")
+# colorFit <- c("BIC"="#bebada", "AIC"="#8dd3c7", "deviance"="#ffffb3") 
+# colorFit <- c("BIC"="#8da0cb", "AIC"="#d95f02", "deviance"="#b2df8a")
+
 
 p <- ggplot2::ggplot(ds, aes(x= reorder(model, value), y=value, fill= Coefficient, group=model))
-p <- p + geom_bar( stat="identity", position="dodge")
+p <- p + geom_bar( stat="identity", position="dodge", alpha=.5)
                   
-p<- p + scale_fill_manual(values=colorFit1)
+p<- p + scale_fill_manual(values=colorFit)
 p<- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p<- p + scale_x_discrete(limits=modelList2)
+# p<- p + scale_y_continuous( limits = c(80000, 110000))
 p
-
-# p <- ggplot2::ggplot(ds, aes(x=model, y=value, fill= Coefficient, group=model))
-# p <- p + geom_bar( data= dplyr::filter(ds,Coefficient=="BIC"),stat="identity", 
-#                    alpha=1 )
-# p <- p + geom_bar( data= dplyr::filter(ds,Coefficient=="AIC"),stat="identity", 
-#                    alpha=1)
-# p <- p + geom_bar( data= dplyr::filter(ds,Coefficient=="deviance"),stat="identity", 
-#                    alpha=1)
-# p<- p + scale_fill_manual(values=colorFit1)
-# p<- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-# p
-
-str(ds)
-
-
-
-breadth_data <- read.table(textConnection("Stakeholder  Value
-                                          'Grantseekers'  0.90
-                                          'Donors'    0.89
-                                          'Community' 0.55
-                                          'Hurricane Relief Fund' 0.24
-                                          'Media' 0.19
-                                          'Employment Seekers'    0.12
-                                          'Affiliates'    0.10
-                                          'Youth' 0.09
-                                          'Women' 0.02
-                                          'Former Board Members'  0.01"), header=TRUE)
-
-breadth_data
-
-
-c <- ggplot(breadth_data, aes(x=Stakeholder, y=Value))
-c + geom_bar(stat="identity") + coord_flip() + scale_y_continuous('') + scale_x_discrete('')
-
-
-
-breadth_data <- transform(breadth_data, 
-                          Stakeholder = reorder(Stakeholder, Value))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
