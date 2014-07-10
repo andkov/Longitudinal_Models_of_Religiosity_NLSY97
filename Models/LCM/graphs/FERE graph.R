fb7c32068be967e1825153f17c43ff3f256524a8
Will Beasley <wibeasley@hotmail.com>
2014-07-10 00:22:12 -0500

    tidy

--- Models/LCM/graphs/FERE graph.R
+++ Models/LCM/graphs/FERE graph.R
@@ -18,44 +18,27 @@ BuildFERE <- function( modelName, dsWide ) {
   
   borderCode <- c(0,0,0,0,2,2,2,2,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,99,99,99,99,0,0,0,0,99,99,99,99,0,0,0,0,99,99,99,99,0,0,0,0,99,99,99,99,3,99,99,99,99,99,99,99)
   fillCode <- c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,3,3,3,3,99,99,99,99,3,3,3,3,99,99,99,99,3,3,3,3,99,99,99,99,3,3,3,3,99,99,99,99,3,99,99,99,99,99,99,99)
-  
-  
-  # run sequenceLCM.R if don't get some objcets
-  #   modelName <- "m6R3"
-  # pathdsFERE  <- file.path("./Models/LCM/models/datasets", paste0(modelName, "_FERE.rds"))
-  # m6R3_FERE<- readRDS(pathdsFERE)
-  # 
-  # dsWide<- m6R3_FERE
 
-  #dsWide <- dsWide %>% dplyr::select( Coefficient, Estimate, Std.Error, t.value, sdRE,intVarRE, timecVarRE, timec2VarRE, sigma )
   columnNamesWide <- c("Coefficient", "Estimate", "Std.Error", "t.value", "sdRE", "intVarRE", "timecVarRE", "timec2VarRE", "timec3VarRE", "sigma")
   dsWide <- dsWide[, columnNamesWide]
 
-  # head(dsWide,10)
   # I will enforce this order, it's important
   target <- c("(Intercept)", "timec", "timec2", "timec3", "cohort", "timec:cohort", "timec2:cohort", "timec3:cohort")
   dsWide<-dsWide[match(target, dsWide$Coefficient), ]
-  # dsWide
-#   dsHeader <- dsWide
-#   dsWide2 <- 
-#   
+
   ds <- melt(dsWide, id.vars=("Coefficient"), value.name="value") 
-  # head(ds, 10)
-  
-  # roundingDigits<- 2
-  # ds <- ds %>% mutate(label= as.character(round(value,roundingDigits))) #I don't think there's a need to use mutate here, but some people do.
-  ds$label <- sprintf("% .2f", ds$value) #format(x=round(ds$value,roundingDigits), trim=FALSE)
+
+  ds$label <- sprintf("% .2f", ds$value) #format(x=round(ds$value,2), trim=FALSE)
   ds$label[is.na(ds$value)] <- ""
-  ds$borderCode <- factor(ifelse(!is.na(ds$value), borderCode, 99))
-  ds$fillCode <- factor(ifelse(!is.na(ds$value), fillCode, 99))
   
   uniqueCoefficientCount <- 8
   uniqueVariableCount <- length(unique(ds$variable))
   ds$row <- rep(x=seq_len(uniqueCoefficientCount), times=uniqueVariableCount)
   ds$col <- rep(x=seq_len(uniqueVariableCount), each=uniqueCoefficientCount)
+  
+  ds$borderCode <- factor(ifelse(!is.na(ds$value), borderCode, 99))
+  ds$fillCode <- factor(ifelse(!is.na(ds$value), fillCode, 99))
     
-  # ggplot(ds, aes(x=col, xmin=col-.5, xmax=col+.5, y=-row, ymin=-row-.5, ymax=-row+.5, label=label)) +
-  #   geom_rect(aes(color=borderCode, fill=fillCode)) +
   g <- ggplot(ds, aes(x=col,y=-row, label=label)) +
     geom_tile(aes(color=borderCode, fill=fillCode)) +
     geom_text(na.rm=T, color="black", hjust=.5, vjust=.5, size=5, family="mono") +
@@ -63,6 +46,7 @@ BuildFERE <- function( modelName, dsWide ) {
     scale_fill_manual(values=paletteFill) +
     emptyTheme +
     theme(legend.position="none")
-  # ggsave(filename="./Models/LCM/graphs/equationTiles.png", plot=last_plot())
+  
+  # ggsave(filename="./Models/LCM/graphs/equationTiles.png", plot=g)
   return( g )
 }
