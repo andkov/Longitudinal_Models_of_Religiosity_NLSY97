b17b574aedc61932668a7a1cc253aa8c071ce143
Will Beasley <wibeasley@hotmail.com>
2014-07-12 14:05:22 -0500

    cleaning up theme & removing gray background

--- Models/LCM/graphs/line graph of trajectories.R
+++ Models/LCM/graphs/line graph of trajectories.R
@@ -28,9 +28,22 @@ BuildLine <- function( modelName, baseSize=12 ) {
   ###############################################
   ### graph of prediction
   
-  bgColour<-gray(.95)   # background color
-  indLineSz<-.08        # individual line size
-  indLineAl<-.06        # individual line alpha
+  #bgColour<-gray(.95)   # background color
+  indLineSz <-.08        # individual line size
+  indLineAl <-.06        # individual line alpha
+  
+  themeLine <- ggplot2::theme_bw(base_size=baseSize) +
+    ggplot2::theme(title=ggplot2::element_text(colour="gray20",size = 12)) +
+    ggplot2::theme(axis.text=ggplot2::element_text(colour="gray40")) +
+    ggplot2::theme(axis.title=ggplot2::element_text(colour="gray40")) +
+    ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80")) +
+    ggplot2::theme(axis.ticks.length = grid::unit(0, "cm")) +
+    ggplot2::theme(text = element_text(size =25)) +
+    # ggplot2::theme(panel.background=element_rect(fill=bgColour,colour=NA)) +
+    ggplot2::theme(legend.position=c(.95,.90),legend.direction="vertical") +
+    ggplot2::theme(legend.background = element_rect(fill=NA)) +
+    ggplot2::theme(legend.text = element_text(size = 15),legend.title.align =(-3.3))# +
+    # ggplot2::theme(panel.grid = element_line(linetype = 1,size=rel(3)))
   
   ds<- dsp %>% dplyr::filter(id %in% c(1:500))
   head(ds)
@@ -38,30 +51,11 @@ BuildLine <- function( modelName, baseSize=12 ) {
   # geoms
   p<- p + geom_line(aes(x=timec,y=yHat),colour="red",alpha=indLineAl,size=indLineSz, na.rm=T)
   p<- p + geom_line(aes(y=yFE), fill=NA, na.rm=T)
-  # p<- p + geom_line(aes(y=gamma00),fill=NA, color="black",size=2)
-  # p<- p + geom_line(aes(y=gamma01),fill=NA, color="red", size=2)
-  # p<- p + geom_line(aes(y=gamma02),fill=NA, color="green", size=2)
-  # p<- p + geom_line(aes(y=gamma03),fill=NA, color="blue", size=2)
-  # p<- p + geom_line(aes(y=gamma10),fill=NA, color="black", size=1, linetype="dashed")
-  # p<- p + geom_line(aes(y=gamma11),fill=NA, color="red", size=1, linetype="dashed")
-  # p<- p + geom_line(aes(y=gamma12),fill=NA, color="green", size=1, linetype="dashed")
-  # p<- p + geom_line(aes(y=gamma13),fill=NA, color="blue",  size=1, linetype="dashed")
   # scales
-  p<- p + scale_x_continuous(breaks=seq(0,10, 1),limits=c(0,10)) 
-  p<- p + scale_y_continuous(breaks=seq(0, 8, 1),limits=c(.5,8.5)) 
+  p <- p + scale_x_continuous(breaks=seq(0,10, 1),limits=c(0,10)) 
+  p <- p + scale_y_continuous(breaks=seq(0, 8, 1),limits=c(.5,8.5)) 
   # themes, guide, and annotations
-  p <- p + ggplot2::theme_bw()
-  p <- p + ggplot2::theme_bw(base_size=baseSize)
-  p <- p + ggplot2::theme(title=ggplot2::element_text(colour="gray20",size = 12)) 
-  p <- p + ggplot2::theme(axis.text=ggplot2::element_text(colour="gray40"))
-  p <- p + ggplot2::theme(axis.title=ggplot2::element_text(colour="gray40"))
-  p <- p + ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80"))
-  p <- p + ggplot2::theme(axis.ticks.length = grid::unit(0, "cm"))
-  p <- p + theme(text = element_text(size =25), panel.background=element_rect(fill=bgColour,colour=NA))
-  p<- p + ggplot2::theme(legend.position=c(.95,.90),legend.direction="vertical")
-  p<- p + ggplot2::theme(legend.background = element_rect(fill=NA))
-  p<- p + ggplot2::theme(legend.text = element_text(size = 15),legend.title.align =(-3.3))
-  p<- p + ggplot2::theme(panel.grid = element_line(linetype = 1,size=rel(3)))
+  p <- p + themeLine 
   p <- p + labs(title="How often have you attended a worship service (2000)?", 
                 x="Church attendance", 
                 y="Count")
