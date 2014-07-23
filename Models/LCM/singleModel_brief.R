9436aa50fb2cabb5f8487c1421e50d3b77fb1c92
Andrey Koval <koval.andrey@gmail.com>
2014-07-23 10:50:44 -0500

    this is why the black line was at 1981

--- Models/LCM/singleModel_brief.R
+++ Models/LCM/singleModel_brief.R
@@ -45,7 +45,7 @@ ds<- dsL %>%  # chose conditions to apply in creating dataset for modeling
             timec= age-16, # metric of time is bilogical age in years, centered at 16
             timec2= timec^2,
             timec3= timec^3,# 
-    cohort=byear-1980-1) %>% # age difference, years younger (unit - 1 cohort away)
+    cohort=byear-1980) %>% # age difference, years younger (unit - 1 cohort away)
   dplyr::select( # assemble the dataset for modeling
     id, sample, race, byear,cohort, # Time Invariant variables
     year,
